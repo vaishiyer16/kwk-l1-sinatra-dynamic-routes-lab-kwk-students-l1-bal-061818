35ba026cf32ddddaa23b4b318a2a4b5d1e42a47a
vaishiyer16 <vaishnavi.l.iyer@gmail.com>
2018-06-26 19:44:34 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   get '/reversename/:name' do
   @user_name = params[:name]
-  @user_name.reverse 
+  @user_name.reverse
   end
   get '/square/:number1/:number2' do
     number1 = params[:number1].to_i
@@ -12,15 +12,15 @@ class App < Sinatra::Base
   end
   get '/say/:number/:phrase' do
     @number = params[:number].to_i
-    @phrase = params[:phrase].to.i 
+    @phrase = params[:phrase].to.i
     @phrase * @number
   end
   get '/say/:word1/:word2/:word3/:word4/:word5' do
-    
   end
-  
-  
-    
+end
+
+
+
   end
 
 end
